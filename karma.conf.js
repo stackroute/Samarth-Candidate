301af36f58469d6bd9e5ad0f7233fec33968d5c7
promit2014 <promit.2014@gmail.com>
2016-10-26 15:47:46 +0530

    implemented angular test frame for candidate

---
+++ karma.conf.js
@@ -0,0 +1,81 @@
+// Karma configuration
+// Generated on Wed Oct 26 2016 15:17:56 GMT+0530 (IST)
+
+module.exports = function(config) {
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/angular-animate/angular-animate.js',
+            'bower_components/angular-aria/angular-aria.js',
+            'bower_components/angular-material/angular-material.js',
+            'bower_components/angular-messages/angular-messages.js',
+            'bower_components/angular-ui-router/release/angular-ui-router.js',
+            'bower_components/angular-flash-alert/dist/angular-flash.min.js',
+            'bower_components/angular-local-storage/dist/angular-local-storage.js',
+            'bower_components/satellizer/dist/satellizer.js',
+            /*'webapp/smcandidateprofile.js',*/
+            'webapp/*.js',
+            'webapp/**/*.js',
+            'test/**/*.js'
+        ],
+
+
+        // list of files to exclude
+        exclude: [],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    })
+}
