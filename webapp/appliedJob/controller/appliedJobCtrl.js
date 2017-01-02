f0ea248920654b690507f3e1791e48ddc1a9533d
thirdperspective <hariprasath.er@gmail.com>
2017-01-02 09:41:50 +0530

    offers message and join and decline is added

--- webapp/appliedJob/controller/appliedJobCtrl.js
+++ webapp/appliedJob/controller/appliedJobCtrl.js
@@ -6,7 +6,11 @@
    'Pagination',
    'appliedJobFactory',
    '$rootScope',
-   function($scope, $stateParams, Pagination,appliedJobFactory,$rootScope) {
+   "offeredDetails",
+   "joinFactory",
+   'declineFactory',
+   "$mdDialog",
+   function($scope, $stateParams, Pagination,appliedJobFactory,$rootScope,offeredDetails,joinFactory,declineFactory,$mdDialog) {
     $scope.candidateid=$rootScope.candidate.candidateid;
 
     appliedJobFactory.appliedJobs($scope.candidateid)
@@ -15,11 +19,72 @@
         console.log($scope.results);
         console.log("results:values"+$scope.candidateid);
         console.log(results);
+
+        offeredDetails.offers($scope.candidateid) 
+        .then(function(results)
+        {
+          $scope.results.forEach(function(job)
+          {
+            results.data.forEach(function(offer){
+            if(job.jb.jobcode==offer.jobcode)
+            {
+              job.jb.offer=true;
+            }
+            })
+          })
+          $scope.offersResult = results.data;
+          if(results.data.length>0)
+          {
+            alert("congrats! you are selected in "+results.data[0].jobcode);
+          }
+          
+        })
+        .catch(function(err){console.log(err)})
+
         $scope.pagination = Pagination.getNew(3);
         $scope.pagination.numPages = Math.ceil(results.data.length / $scope.pagination.perPage);
       }, function err(err) {
         $scope.message = err;
       });
+
+    $scope.join=function(jobcode){
+
+      var confirm = $mdDialog.confirm()
+          .title('Apply')
+          .textContent('You are about to join!, please confirm..!')
+          .ok('Confirm')
+          .cancel('Cancel');
+
+      $mdDialog.show(confirm).then(function() {
+        joinFactory.join($scope.candidateid,jobcode)
+        .then(function successCallbackfun(response){
+          console.log(response);
+        },function errorCallbackfun(error){
+          console.log(error);
+        })
+        .catch(function(err){console.log(err)})
+      })
+    }
+
+    $scope.decline=function(jobcode){
+
+      var confirm = $mdDialog.confirm()
+          .title('Apply')
+          .textContent('You are about to decline job!, please confirm..!')
+          .ok('Confirm')
+          .cancel('Cancel');
+
+      $mdDialog.show(confirm).then(function() {
+        declineFactory.decline($scope.candidateid,jobcode)
+        .then(function successCallbackfun(response){
+          console.log(response);
+        },function errorCallbackfun(error){
+          console.log(error);
+        })
+        .catch(function(err){console.log(err)})
+      })
+    }
+      
   }
   ])
 })()
--- webapp/appliedJob/factory/appliedJobFactory.js
+++ webapp/appliedJob/factory/appliedJobFactory.js
@@ -13,4 +13,49 @@ angular
    }
    return obj;
 	}])
+
+  .factory("offeredDetails", ['$http',function($http) {
+   var obj = {};
+   obj.offers = function(candidateid){
+        console.log("it is calling the offered detail jobs")
+       return $http({
+       method : 'GET',
+       url : 'placementprocess/offeredDetails/'+candidateid
+       })
+   }
+   return obj;
+  }])
+
+  .factory("joinFactory", ['$http',function($http) {
+   var obj = {};
+   obj.join = function(cid,jobcode){
+        console.log("it is calling the joinjobs"+jobcode+cid)
+       return $http({
+       method : 'POST',
+       url : '/placementprocess/join',
+       data:{
+        candidateid:cid,
+        jobcode:jobcode
+       }
+       })
+   }
+   return obj;
+  }])
+
+  .factory("declineFactory", ['$http',function($http) {
+   var obj = {};
+   obj.decline = function(cid,jobcode){
+        console.log("it is calling the declinejobs"+jobcode+cid)
+       return $http({
+       method : 'POST',
+       url : '/placementprocess/decline',
+       data:{
+        candidateid:cid,
+        jobcode:jobcode
+       }
+       })
+   }
+   return obj;
+  }])
+
 })();
